f57f7d0255a4ad6a0c2472ae5631331c345fd45e
Iordache Ana-Maria <anaiordache264@gmail.com>
2022-03-02 23:37:18 +0200

    added activities for doctor, AdminActivity and other additions

---
+++ app/src/main/java/eu/ase/medicalapplicenta/activitati/AdminActivity.java
@@ -0,0 +1,56 @@
+package eu.ase.medicalapplicenta.activitati;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.ase.medicalapplicenta.R;
+import eu.ase.medicalapplicenta.entitati.Investigatie;
+import eu.ase.medicalapplicenta.entitati.Specialitate;
+
+public class AdminActivity extends AppCompatActivity {
+    EditText et1, et2, et3;
+    Button button1, button2;
+    List<Investigatie> investigatii = new ArrayList<>();
+
+    private final DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Specialitati");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+        et3=findViewById(R.id.et3);
+        button1=findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Investigatie i=new Investigatie(et2.getText().toString(),Double.parseDouble(et3.getText().toString()));
+                investigatii.add(i);
+            }
+        });
+        button2=findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Specialitate specialitate = new Specialitate(null, et1.getText().toString(),investigatii);
+                String idSpecialitate = reference.push().getKey();
+                specialitate.setIdSpecialitate(idSpecialitate);
+                reference.child(specialitate.getIdSpecialitate()).setValue(specialitate);
+            }
+        });
+
+
+    }
+}
